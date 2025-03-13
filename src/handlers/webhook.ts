db36c7ce7cdee0ff140b5983fea08c103b532124
Dmitry Malofeev <dim2k2006@yandex.ru>
2025-03-13 16:17:32 +0000

    Update webhook handler

--- src/handlers/webhook.ts
+++ src/handlers/webhook.ts
@@ -1,14 +1,38 @@
 import { Container } from '../container';
-import { HttpResponseInit, HttpRequest } from '@azure/functions';
+import { HttpResponseInit, HttpRequest, InvocationContext } from '@azure/functions';
+import { webhookCallback } from 'grammy';
+import buildBot from '../bot';
 
 export function makeWebhookHandler(container: Container) {
-  return async function webhook(request: HttpRequest): Promise<HttpResponseInit> {
+  return async function webhook(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     try {
       const rawBody = await request.json();
 
-      await container.queueProvider.sendMessage(JSON.stringify(rawBody));
+      const bot = buildBot(container);
 
-      return { status: 200, body: 'accepted' };
+      const modifiedRequest = {
+        ...request,
+        body: rawBody,
+      };
+
+      const handleUpdate = webhookCallback(bot, 'azure');
+
+      return await new Promise<HttpResponseInit>((resolve, reject) => {
+        const replyAdapter = {
+          res: {
+            status: 200,
+            body: '',
+            send: (data: unknown) => {
+              context.log('data:', data);
+              resolve({ body: data as HttpResponseInit['body'] });
+            },
+          },
+        };
+
+        handleUpdate(modifiedRequest, replyAdapter)
+          .then(() => resolve({ body: 'ok' }))
+          .catch(reject);
+      });
     } catch (error) {
       container.exceptionProvider.captureException(error, 'Failed to receive telegram webhook.');
 
