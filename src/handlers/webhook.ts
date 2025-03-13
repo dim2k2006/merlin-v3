76acbc3542d0d088dea6f294e4e5aa887ef4d9b7
Dmitry Malofeev <dim2k2006@yandex.ru>
2025-03-13 14:57:05 +0000

    Add webhook handler

--- src/handlers/webhook.ts
+++ src/handlers/webhook.ts
@@ -1,12 +1,19 @@
 import { Container } from '../container';
-import { HttpResponseInit, HttpRequest } from '@azure/functions';
+import { HttpResponseInit, HttpRequest, InvocationContext } from '@azure/functions';
 import { webhookCallback } from 'grammy';
 import buildBot from '../bot';
 
 export function makeWebhookHandler(container: Container) {
-  return async function webhook(request: HttpRequest): Promise<HttpResponseInit> {
+  return async function webhook(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     try {
       const bot = buildBot(container);
+
+      const rawBody = await request.json();
+
+      const modifiedRequest = {
+        ...request,
+        body: rawBody,
+      };
 
       const handleUpdate = webhookCallback(bot, 'azure');
 
@@ -16,12 +23,13 @@ export function makeWebhookHandler(container: Container) {
             status: 200,
             body: '',
             send: (data: unknown) => {
+              context.log('data:', data);
               resolve({ body: data as HttpResponseInit['body'] });
             },
           },
         };
 
-        handleUpdate(request, replyAdapter).catch(reject);
+        handleUpdate(modifiedRequest, replyAdapter).catch(reject);
       });
     } catch (error) {
       container.exceptionProvider.captureException(error, 'Failed to process telegram webhook.');
