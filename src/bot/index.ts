e865ef71ca514cb91e20d7badde2d82a8b005304
Dmitry Malofeev <dim2k2006@yandex.ru>
2025-03-13 16:31:32 +0000

    Update webhook handler

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -118,65 +118,6 @@ function buildBot(container: Container) {
     await container.queueProvider.sendMessage(JSON.stringify({ chatId: toString(chatId), message, threadId, user }));
 
     await ctx.react('👀');
-
-    try {
-      const agentResponse = await container.agentProvider.invoke(
-        {
-          messages: [
-            container.agentProvider.buildChatMessage({
-              role: 'developer',
-              content: `User Info: id=${user.id}, externalId=${user.externalId}, firstName=${user.firstName}, lastName=${user.lastName}.
-Please generate a clear, concise answer to the user's query. At the end of your response, add a line "Tools Used:" followed by the names of any tools that were utilized. If no tool was used, output "none". It super puper duper important to attach valid information about tools used! Give it high priority!`,
-            }),
-            container.agentProvider.buildChatMessage({
-              role: 'user',
-              content: message,
-            }),
-          ],
-        },
-        { threadId },
-      );
-
-      const replyMessage = agentResponse.messages[agentResponse.messages.length - 1];
-
-      if (!replyMessage) {
-        await ctx.reply('I do not understand what you are saying. 😔');
-
-        return;
-      }
-
-      await ctx.reply(replyMessage.content);
-    } catch (error) {
-      const errorMessage = get(error, 'message', 'An error occurred.');
-
-      const agentResponse = await container.agentProvider.invoke(
-        {
-          messages: [
-            container.agentProvider.buildChatMessage({
-              role: 'developer',
-              content: `The system encountered an error while processing the user's request.
-User Info: id=${user.id}, externalId=${user.externalId}, firstName=${user.firstName}, lastName=${user.lastName}.
-User's message: "${message}".
-Error details: "${errorMessage}".
-Please provide a clear, concise explanation of the error in plain language that a non-technical user can understand.
-Also, suggest what the user might do next, such as trying again later or contacting support if the problem persists.
-At the end, add a line "Tools Used:" and list any tools that were involved in handling this request. If no tools were used, output "none".`,
-            }),
-          ],
-        },
-        { threadId },
-      );
-
-      const replyMessage = agentResponse.messages[agentResponse.messages.length - 1];
-
-      if (!replyMessage) {
-        await ctx.reply('An error occurred.');
-
-        return;
-      }
-
-      await ctx.reply(replyMessage.content);
-    }
   });
 
   return bot;
