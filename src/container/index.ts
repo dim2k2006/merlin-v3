81dbb17e43f8666fd14071f88dd00d94dee12ac6
Dmitry Malofeev <dim2k2006@yandex.ru>
2025-03-13 15:24:13 +0000

    Update container

--- src/container/index.ts
+++ src/container/index.ts
@@ -18,6 +18,7 @@ function getEnvVariable(name: string): string {
 
 export function buildConfig(): Config {
   const nodeEnv = getEnvVariable('NODE_ENV');
+  const storageConnectionString = getEnvVariable('AZURE_STORAGE_CONNECTION_STRING');
   const cosmosDbEndpoint = getEnvVariable('COSMOS_DB_ENDPOINT');
   const cosmosDbKey = getEnvVariable('COSMOS_DB_KEY');
   const pineconeApiKey = getEnvVariable('PINECONE_API_KEY');
@@ -29,6 +30,7 @@ export function buildConfig(): Config {
   const correlateWebAppUrl = getEnvVariable('CORRELATE_WEB_APP_URL');
 
   return {
+    storageConnectionString,
     cosmosDbEndpoint,
     cosmosDbKey,
     cosmosDbName: 'merlin',
@@ -46,6 +48,7 @@ export function buildConfig(): Config {
 }
 
 export type Config = {
+  storageConnectionString: string;
   cosmosDbEndpoint: string;
   cosmosDbKey: string;
   cosmosDbName: string;
